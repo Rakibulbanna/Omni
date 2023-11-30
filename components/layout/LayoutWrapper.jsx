17c49a5179a0ba106741d5277627631719ba9a96
Rakib ul banna <135686564+Rakib-ul-Banna-Elitbuzz@users.noreply.github.com>
2023-11-30 14:56:01 +0600

    icon added

--- components/layout/LayoutWrapper.jsx
+++ components/layout/LayoutWrapper.jsx
@@ -19,12 +19,13 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import { ExpandLess, ExpandMore } from '@mui/icons-material';
-import StarBorder from '@mui/icons-material/StarBorder';
+import LogoutIcon from '@mui/icons-material/Logout';
 import { Collapse, Grid } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
 import GroupIcon from '@mui/icons-material/Group';
 import Link from 'next/link';
 import Image from 'next/image';
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import { Fragment, useState } from 'react';
 import Footer from './Footer';
 const drawerWidth = 200;
@@ -58,7 +59,8 @@ const DrawerHeader = styled('div')(({ theme }) => ({
     ...theme.mixins.toolbar,
 }));
 
-const AppBar = styled(MuiAppBar, {shouldForwardProp: (prop) => prop !== 'open',
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', 'margin'], {
@@ -88,12 +90,13 @@ const AppBar = styled(MuiAppBar, {shouldForwardProp: (prop) => prop !== 'open',
 
     }),
 }));
-const FooterBar = styled(MuiAppBar, {shouldForwardProp: (prop) => prop !== 'open',
+const FooterBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
     zIndex: theme.zIndex.drawer + 1,
-    bottom:0,
-    top:`calc(100% - ${theme.spacing(5)})`,
-   
+    bottom: 0,
+    top: `calc(100% - ${theme.spacing(5)})`,
+
     ...(!open && {
         [theme.breakpoints.up('xs')]: {
             width: `calc(100% - ${theme.spacing(7)})`,
@@ -108,7 +111,7 @@ const FooterBar = styled(MuiAppBar, {shouldForwardProp: (prop) => prop !== 'open
     ...(open && {
         marginLeft: drawerWidth,
         width: `calc(100% - ${drawerWidth}px)`,
-        
+
 
         transition: theme.transitions.create(['width', 'margin'], {
             easing: theme.transitions.easing.sharp,
@@ -147,71 +150,84 @@ export default function LayoutWrapper({ children }) {
 
     return (
         <>
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
 
-            <AppBar position="fixed" open={open} sx={{ backgroundColor: '#33ab9f', border: 0 }}>
-                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                <AppBar position="fixed" open={open} sx={{ backgroundColor: '#33ab9f', border: 0 }}>
+                    <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
 
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerControl}
-                        edge="start"
-                        sx={{
-                            marginRight: 5,
-                        }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Grid display={'grid'} gridTemplateColumns={'1fr 1fr 1fr'} gap={4}>
-                        <Link href='/'>Accounts</Link>
-                        <Link href='/' >Logout</Link>
-                        <Link href='/' >Modules</Link>
-                    </Grid>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={handleDrawerControl}
+                            edge="start"
+                            sx={{
+                                marginRight: 5,
+                            }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Grid display={'grid'} gridTemplateColumns={'1fr 1fr 1fr'} gap={3}>
+                            <Link href='/'>
+                                <ListItemText primary={'Accounts'} />
+                            </Link>
+                            <Link href='/'>
+                                <span style={{ display: 'flex', gap: 3, alignItems: 'center' }}>
+                                    <LogoutIcon sx={{ color: 'white' }} />
+                                    <ListItemText primary={`Logout`} />
+                                </span>
+                            </Link>
+                            <Link href='/'>
+                                <span style={{ display: 'flex', gap: 3, alignItems: 'center' }}>
+                                    <FormatListBulletedIcon sx={{ color: 'white' }} />
+                                    <ListItemText primary={`Modules`} />
+                                </span>
+                            </Link>
 
-                </Toolbar>
-            </AppBar>
-
-            <Drawer variant="permanent" open={open}>
-                <Link href='/'>
-                {
-                    open ? <Grid sx={{ display: 'flex', justifyContent: 'start', py: 2, pl: 2, backgroundColor: '#33ab9f', color: 'white' }}>
-                        <Grid display={'flex'} flexDirection={'column'} gap={1.5}>
-                            <Image src={'/dumy_teacher.png'}
-                                height={60}
-                                width={60}
-                                alt='Student photo'
-                                loading='lazy'
-                                style={{
-                                    borderRadius: '50%'
-                                }}
-                            />
-                            <Typography>Omni Solution Ltd</Typography>
                         </Grid>
-                    </Grid>
-                        :
-                        <Grid sx={{
-                            backgroundColor: '  ', color: 'white',
-                            height: '1fr', padding: 1,
-                        }}>
-                            <img src={'/omni.jpeg'}
 
-                                alt='Student photo'
-                                loading='lazy'
-                                style={{
-                                    borderRadius: '50%',
-                                    maxHeight: '100%',
-                                    border: '1px solid',
-                                }}
-                            />
-                        </Grid>
-                }
-                </Link>
+                    </Toolbar>
+                </AppBar>
 
-                <Divider />
-                <List>
-                    {/* {['Inbox', 'Starred'].map((text, index) => (
+                <Drawer variant="permanent" open={open}>
+                    <Link href='/'>
+                        {
+                            open ? <Grid sx={{ display: 'flex', justifyContent: 'start', py: 2, pl: 2, backgroundColor: '#33ab9f', color: 'white' }}>
+                                <Grid display={'flex'} flexDirection={'column'} gap={1.5}>
+                                    <Image src={'/dumy_teacher.png'}
+                                        height={60}
+                                        width={60}
+                                        alt='Student photo'
+                                        loading='lazy'
+                                        style={{
+                                            borderRadius: '50%'
+                                        }}
+                                    />
+                                    <Typography>Omni Solution Ltd</Typography>
+                                </Grid>
+                            </Grid>
+                                :
+                                <Grid sx={{
+                                    backgroundColor: '  ', color: 'white',
+                                    height: '1fr', padding: 1,
+                                }}>
+                                    <img src={'/omni.jpeg'}
+
+                                        alt='Student photo'
+                                        loading='lazy'
+                                        style={{
+                                            borderRadius: '50%',
+                                            maxHeight: '100%',
+                                            border: '1px solid',
+                                        }}
+                                    />
+                                </Grid>
+                        }
+                    </Link>
+
+                    <Divider />
+                    <List>
+                        {/* {['Inbox', 'Starred'].map((text, index) => (
                         <ListItem key={text} disablePadding sx={{ display: 'block' }}>
                             <ListItemButton
                                 sx={{
@@ -233,30 +249,32 @@ export default function LayoutWrapper({ children }) {
                             </ListItemButton>
                         </ListItem>
                     ))} */}
-                    <ListItem disablePadding sx={{ display: 'block' }}>
-                        <MenuWithSubmenu
-                            menu={{ title: 'Settings', icon: <SettingsIcon /> }}
-                            subMenu={[
-                                { title: 'Group Entry', icon: <GroupIcon />,link:'/group-entry' },
-                            ]}
-                        />
-                    </ListItem>
+                        <ListItem disablePadding sx={{ display: 'block' }}>
+                            <MenuWithSubmenu
+                                menu={{ title: 'Settings', icon: <SettingsIcon /> }}
+                                subMenu={[
+                                    { title: 'Group Entry', icon: <GroupIcon />, link: '/group-entry' },
+                                ]}
+                            />
+                        </ListItem>
 
 
-                </List>
+                    </List>
 
-            </Drawer>
-            <Box component="main" sx={{ flexGrow: 1, p: 2, width: '100%',
-            backgroundColor:'#f5f5f5'}}>
-                <DrawerHeader />
-                <Grid sx={{ minHeight: 'calc(100vh - 100px) !important' }}>
-                {children}
-                </Grid>
+                </Drawer>
+                <Box component="main" sx={{
+                    flexGrow: 1, p: 2, width: '100%',
+                    backgroundColor: '#f5f5f5'
+                }}>
+                    <DrawerHeader />
+                    <Grid sx={{ minHeight: 'calc(100vh - 100px) !important' }}>
+                        {children}
+                    </Grid>
+                </Box>
             </Box>
-        </Box>
-        <FooterBar position="fixed" open={open} sx={{ backgroundColor: 'white',color:'black', border: '1px solid lightGrey',px:2,py:1 }}>
-            <Footer/>
-        </FooterBar>
+            <FooterBar position="fixed" open={open} sx={{ backgroundColor: 'white', color: 'black', border: '1px solid lightGrey', px: 2, py: 1 }}>
+                <Footer />
+            </FooterBar>
         </>
     );
 }
@@ -280,12 +298,12 @@ const MenuWithSubmenu = ({ menu, subMenu }) => {
                     {
                         subMenu.map(i => (
                             <Link href={i?.link}>
-                            <ListItemButton sx={{ pl: 4 }}>
-                                <ListItemIcon>
-                                    {i?.icon}
-                                </ListItemIcon>
-                                <ListItemText primary={`${i?.title}`} />
-                            </ListItemButton>
+                                <ListItemButton sx={{ pl: 4 }}>
+                                    <ListItemIcon>
+                                        {i?.icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={`${i?.title}`} />
+                                </ListItemButton>
                             </Link>
                         )
                         )
